46ec57cc9981deab6eaba5f39b72ffe28a7f8f3c
Mahmud-007 <mahmud6799@gmail.com>
2024-02-01 19:20:16 +0600

    connect mongo

---
+++ tutorial/tutorial/spiders/qoutes.py
@@ -0,0 +1,24 @@
+import scrapy
+from ..items import TutorialItem
+
+class QoutesSpider(scrapy.Spider):
+    name = "qoutes"
+    allowed_domains = ["toscrape.com"]
+    start_urls = ["https://quotes.toscrape.com/"]
+
+    def parse(self, response):
+        items = TutorialItem()
+        all_div_quotes = response.css("div.quote")
+        
+        for quote in all_div_quotes:
+            title = quote.css("span.text::text").extract()
+            author = quote.css(".author::text").extract()
+            tags = quote.css(".tag::text").extract()
+            items["title"] = title
+            items["author"] = author
+            items["tags"] = tags
+            yield items
+
+
+
+
